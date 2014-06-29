22ddf6c69bbcb5527a6fd29b066341aafcc3a518
Erik Price <erik.price16@gmail.com>
2014-06-29 13:24:56 -0700

    Explicitly mark raw pointers as const

--- src/tweetnacl/bindings.rs
+++ src/tweetnacl/bindings.rs
@@ -22,10 +22,10 @@ extern "C" {
 
     /// Perform scalar multiplication using Curve 25519's field.
     pub fn crypto_scalarmult(result: *mut c_uchar,
-                             n: *c_uchar,
-                             p: *c_uchar) -> c_int;
+                             n: *const c_uchar,
+                             p: *const c_uchar) -> c_int;
     /// Perform scalar multiplication against the Curve 25519 base point.
-    pub fn crypto_scalarmult_base(result: *mut c_uchar, n: *c_uchar) -> c_int;
+    pub fn crypto_scalarmult_base(result: *mut c_uchar, n: *const c_uchar) -> c_int;
 
     // --- Crypto box ---
 
@@ -34,35 +34,35 @@ extern "C" {
 
     /// Sign and encrypt a message using asymmetric encryption.
     pub fn crypto_box(cipher: *mut c_uchar,
-                      msg: *c_uchar,
+                      msg: *const c_uchar,
                       len: c_ulonglong,
-                      nonce: *c_uchar,
-                      pk: *c_uchar,
-                      sk: *c_uchar) -> c_int;
+                      nonce: *const c_uchar,
+                      pk: *const c_uchar,
+                      sk: *const c_uchar) -> c_int;
 
     /// Verify and decrypt a message generated with `crypto_box`.
     pub fn crypto_box_open(msg: *mut c_uchar,
-                           cipher: *c_uchar,
+                           cipher: *const c_uchar,
                            len: c_ulonglong,
-                           nonce: *c_uchar,
-                           pk: *c_uchar,
-                           sk: *c_uchar) -> c_int;
+                           nonce: *const c_uchar,
+                           pk: *const c_uchar,
+                           sk: *const c_uchar) -> c_int;
 
     // --- Secret box ---
 
     /// Symmetrically encrypt a message using a shared secret key.
     pub fn crypto_secretbox(cipher: *mut c_uchar,
-                            msg: *c_uchar,
+                            msg: *const c_uchar,
                             len: c_ulonglong,
-                            nonce: *c_uchar,
-                            k: *c_uchar) -> c_int;
+                            nonce: *const c_uchar,
+                            k: *const c_uchar) -> c_int;
 
     /// Decrypt a message encryped with `crypto_secretbox`.
     pub fn crypto_secretbox_open(msg: *mut c_uchar,
-                                 cipher: *c_uchar,
+                                 cipher: *const c_uchar,
                                  len: c_ulonglong,
-                                 nonce: *c_uchar,
-                                 k: *c_uchar) -> c_int;
+                                 nonce: *const c_uchar,
+                                 k: *const c_uchar) -> c_int;
 
     // --- Sign ---
 
@@ -73,20 +73,20 @@ extern "C" {
     /// Sign a message using a given secret key.
     pub fn crypto_sign(smsg: *mut c_uchar,
                        smsg_len: *mut c_ulonglong,
-                       msg: *c_uchar,
+                       msg: *const c_uchar,
                        msg_len: c_ulonglong,
-                       sk: *c_uchar) -> c_int;
+                       sk: *const c_uchar) -> c_int;
 
     /// Verify the validity of an asymmetrially signed message.
     pub fn crypto_sign_open(msg: *mut c_uchar,
                             msg_len: *mut c_ulonglong,
-                            smsg: *c_uchar,
+                            smsg: *const c_uchar,
                             smsg_len: c_ulonglong,
-                            pk: *c_uchar) -> c_int;
+                            pk: *const c_uchar) -> c_int;
 
     // --- Hash ---
     /// Hash.
     pub fn crypto_hash(hash: *mut c_uchar,
-                       msg: *c_uchar,
+                       msg: *const c_uchar,
                        len: u64) -> c_int;
 }
